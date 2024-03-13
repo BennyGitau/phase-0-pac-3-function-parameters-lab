34bac40499173fcd64628118a6206068e04ad29c
BensonGitau <bensonkamaugitau@gmail.com>
2024-03-13 17:20:22 +0300

    completed lab

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ function introductionWithLanguageOptional(name, language = "JavaScript") {
     
     return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
-introductionWithLanguageOptional("Gracie", "Python");
+introductionWithLanguageOptional(name, "Python");
